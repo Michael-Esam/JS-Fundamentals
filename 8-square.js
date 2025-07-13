89edb2362c53de3503ff973da6e01fd8c8d3832a
Michael Essam <maichelessam2001@gmail.com>
2025-07-13 16:01:00 +0300

    add 8-square.js file

--- 8-square.js
+++ 8-square.js
@@ -1,9 +1,9 @@
 const square = parseInt(process.argv[2]);
 if (isNaN(square)){
-    console.log('Missing size')
-}else{
-    let i =0;
+    console.log('Missing size');
+}else if (square > 0){
+    let i = 0;
     while(i < square){
-        console.log('x'.repeat(square))
+        console.log('x'.repeat(square));
     }
-}
+};
