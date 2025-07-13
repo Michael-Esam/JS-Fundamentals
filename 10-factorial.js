d02ce052328231f428f77fd75da8de39cdbbd7eb
Michael Essam <maichelessam2001@gmail.com>
2025-07-13 17:46:50 +0300

    add 10-factorial.js file

---
+++ 10-factorial.js
@@ -0,0 +1,11 @@
+const num = parseInt(process.argv[2])
+
+const factorial = (num)=>{
+    if (isNaN(num) || num === 0){
+        return 1
+    }else{
+        return num * factorial(num -1 )
+    }
+}
+
+console.log(factorial(num));
