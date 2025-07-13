bdc8b26e788cd68109529bff2a056dfecd246427
Michael Essam <maichelessam2001@gmail.com>
2025-07-13 15:44:55 +0300

    add 7-multi_c.js file

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,10 +1,12 @@
-const x =parseInt(process.argv[2]);
-if (isNaN(x)){
-    console.log("Missing number of occurrences")
-}else{
-    let i = 0;
-    while( x > i ){
-        console.log("C is fun");
-        i++;
-    }
+
+const count = parseInt(process.argv[2]);
+
+if (isNaN(count)) {
+  console.log('Missing number of occurrences');
+} else {
+  let i = 0;
+  while (i < count) {
+    console.log('C is fun');
+    i++;
+  }
 }
