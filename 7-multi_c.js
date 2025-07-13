8a9b8e14a2084dbb4ef6d130b8a6d6e902e8a7cb
Michael Essam <maichelessam2001@gmail.com>
2025-07-13 15:27:03 +0300

    add 7-multi_c.js file

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -2,7 +2,9 @@ const x =process.argv[2];
 if (x === undefined){
     console.log("Missing number of occurrences")
 }else{
-    for(let i=0;x >i; i++){
-        console.log("C is fun")
+    let i;
+    while(x > i){
+        console.log("C is fun");
+        i++;
     }
 }
